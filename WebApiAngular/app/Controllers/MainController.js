71526a067d6c2473f3c74dbc57139eb79c8db05c
pankaj-dhami <reachpankajdhami@gmail.com>
2015-07-28 17:35:45 +0530

    created

---
+++ WebApiAngular/app/Controllers/MainController.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    var MainController = function ($scope, $interval, $location) {
+      
+        $scope.username = 'angular';
+        $scope.countdown = 5;
+        var countDownInterval = null;
+        var startCountDown = function () {
+            countDownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+        var decrementCountdown = function () {
+
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        };
+
+        $scope.search = function (username) {
+         
+            if (countDownInterval) {
+                $interval.cancel(countDownInterval);
+                $scope.countdown = null;
+            }
+            $location.path("/user/" + username);
+        };
+        startCountDown();
+
+
+    };
+    var app = angular.module('AngularApp')
+    app.controller('MainController', ['$scope', '$interval', '$location', MainController]);
+
+
+}());
+
---
+++ WebApiAngular/app/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="Views/Main.html" />
+(function () {
+
+    var app = angular.module('AngularApp', ["ngRoute", 'ngProgressLite']);
+    app.config(['ngProgressLiteProvider', function (ngProgressLiteProvider) {
+        ngProgressLiteProvider.settings.ease = 'ease-in';
+    }]);
+    app.config(function ($routeProvider) {
+        $routeProvider.when("/main", {
+            templateUrl: "/app/Views/Main.html",
+            controller: "MainController"
+        }).when("/user/:username", {
+            templateUrl: "/app/Views/UserDetails.html",
+            controller: "UserController"
+        })
+
+            .otherwise({ redirectTo: "/main" });
+
+    });
+
+}());
